576d6007b13bfba89ef691b94a7834edb825ebe9
Eirik Sjøløkken <e.sjoeloe@gmail.com>
2021-11-23 21:18:50 +0100

    Create UserCreatedEvent.cs

---
+++ src/Infrastructure/Eiromplays.IdentityServer.Identity/Events/User/UserCreatedEvent.cs
@@ -0,0 +1,15 @@
+﻿using Eiromplays.IdentityServer.Domain.Common;
+using Eiromplays.IdentityServer.Identity.Entities;
+
+namespace Eiromplays.IdentityServer.Identity.Events.User
+{
+    public class UserCreatedEvent : DomainEvent
+    {
+        public UserCreatedEvent(ApplicationUser user)
+        {
+            User = user;
+        }
+
+        public ApplicationUser User { get; }
+    }
+}
