e255bda343a17d78fe2c9095fa946d1fcf830cb7
Eirik Sjøløkken <e.sjoeloe@gmail.com>
2022-04-12 23:54:51 +0200

    Update Grant.tsx

--- src/Presentation/Eiromplays.IdentityServer.WebUI/ClientApp/src/features/grants/routes/Grant.tsx
+++ src/Presentation/Eiromplays.IdentityServer.WebUI/ClientApp/src/features/grants/routes/Grant.tsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
 
-import { Spinner } from '@/components/Elements';
+import { MDPreview, Spinner } from '@/components/Elements';
 import { Head } from '@/components/Head';
 import { ContentLayout } from '@/components/Layout';
 import { formatDate } from '@/utils/format';
@@ -32,7 +32,11 @@ export const Grant = () => {
             <div className="bg-white shadow overflow-hidden sm:rounded-lg">
               <div className="px-4 py-5 sm:px-6">
                 <div className="mt-1 max-w-2xl text-sm text-gray-500">
-                  {grantQuery.data.description}
+                  <MDPreview value={grantQuery.data.clientName} />
+                  {grantQuery.data.clientLogoUrl && (
+                    <img src={grantQuery.data.clientLogoUrl} alt="Client logo" className="mt-4" />
+                  )}
+                  <MDPreview value={grantQuery.data.description ?? ''} />
                 </div>
               </div>
             </div>
