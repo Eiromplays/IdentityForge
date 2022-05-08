a14639d3827aa95f8b380f1ec10673194e7fcbf4
Eirik Sjøløkken <e.sjoeloe@gmail.com>
2022-05-08 21:31:21 +0200

    WIP Roles pagination

---
+++ src/Presentation/Eiromplays.IdentityServer.Admin.WebUI/eiromplays-identityserver-admin-webui/src/features/roles/api/searchRoles.ts
@@ -0,0 +1,33 @@
+import {
+  axios,
+  ExtractFnReturnType,
+  QueryConfig,
+  PaginationFilter,
+  PaginationResponse,
+} from 'eiromplays-ui';
+import { useQuery } from 'react-query';
+
+import { Role } from '../types';
+
+export type SearchRoleDTO = PaginationFilter & {
+  enabled?: boolean;
+};
+
+export const searchRoles = (data: SearchRoleDTO): Promise<PaginationResponse<Role>> => {
+  return axios.post('/roles/search', data);
+};
+
+type QueryFnType = typeof searchRoles;
+
+type UseSearchRolesOptions = {
+  data: SearchRoleDTO;
+  config?: QueryConfig<QueryFnType>;
+};
+
+export const useSearchUsers = ({ data, config }: UseSearchRolesOptions) => {
+  return useQuery<ExtractFnReturnType<QueryFnType>>({
+    ...config,
+    queryKey: ['search-roles', data.pageNumber],
+    queryFn: () => searchRoles(data),
+  });
+};
