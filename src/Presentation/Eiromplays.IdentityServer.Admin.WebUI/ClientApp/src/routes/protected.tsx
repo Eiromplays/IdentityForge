91a551c862db60203953618b213bd6c18adf11ee
Eirik Sjøløkken <e.sjoeloe@gmail.com>
2022-05-19 21:07:59 +0200

    Add sideBarNavigationItems to Admin.WebUI

--- src/Presentation/Eiromplays.IdentityServer.Admin.WebUI/ClientApp/src/routes/protected.tsx
+++ src/Presentation/Eiromplays.IdentityServer.Admin.WebUI/ClientApp/src/routes/protected.tsx
@@ -1,6 +1,13 @@
 import { Outlet } from '@tanstack/react-location';
 import { lazyImport, MainLayout, NotAllowed, Spinner } from 'eiromplays-ui';
 import { Suspense } from 'react';
+import {
+  HiOutlineDocumentText,
+  HiOutlineHome,
+  HiOutlineShieldCheck,
+  HiOutlineUsers,
+} from 'react-icons/hi';
+import { MdOutlineDevicesOther, MdOutlineHistory } from 'react-icons/md';
 
 import logo from '@/assets/logo.svg';
 import { LogsRoutes } from '@/features/logs';
@@ -18,7 +25,24 @@ const App = () => {
   if (!checkAccess({ allowedRoles: [ROLES.ADMINISTRATOR] })) return <NotAllowed logo={logo} />;
 
   return (
-    <MainLayout logo={logo}>
+    <MainLayout
+      logo={logo}
+      sideBarNavigationItems={[
+        { name: 'Dashboard', to: '.', icon: HiOutlineHome },
+        { name: 'Dashboard', to: '.', icon: HiOutlineHome },
+        { name: 'Users', to: './users', icon: HiOutlineUsers },
+        { name: 'Persisted Grants', to: './persisted-grants', icon: HiOutlineShieldCheck },
+        { name: 'User Sessions', to: './user-sessions', icon: MdOutlineDevicesOther },
+        { name: 'Logs', to: './logs', icon: MdOutlineHistory },
+        {
+          name: 'Discovery Document',
+          to: 'https://localhost:7001/.well-known/openid-configuration',
+          target: '_blank',
+          externalLink: true,
+          icon: HiOutlineDocumentText,
+        },
+      ]}
+    >
       <Suspense
         fallback={
           <div className="h-full w-full flex items-center justify-center">
